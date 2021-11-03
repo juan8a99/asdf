bcaca24b86d8a0b199f0b47cba3fb119d5d8e7c8
juan8a99 <70856052+juan8a99@users.noreply.github.com>
2021-11-03 15:35:25 -0500

    Add files via upload

---
+++ dynamic/index.php
@@ -0,0 +1,7 @@
+﻿<style type="text/css">
+
+</style>
+
+
+<p align="center" > <img src="images/bustofus.jpg" alt="bustofus" width="268" height="206" align="middle" longdesc="images/bustofus.jpg" /></p>
+
